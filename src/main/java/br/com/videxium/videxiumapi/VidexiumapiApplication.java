767811c0805f22f0d1c8ddea5123d66743157ef2
Credentium (Desenvolvimento) <credentium@outlook.com.br>
2025-12-17 11:11:20 -0300

    init: Versão Inicial do Projeto

---
+++ src/main/java/br/com/videxium/videxiumapi/VidexiumapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.videxium.videxiumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidexiumapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidexiumapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/videxium/videxiumapi/VidexiumapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.videxium.videxiumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VidexiumapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
